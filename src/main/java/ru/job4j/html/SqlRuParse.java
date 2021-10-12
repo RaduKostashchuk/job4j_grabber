3ba7c61e46961df582fb32ace0d3065eb8d853b2
RaduKostashchuk <kostasc@mail.ru>
2021-10-12 14:14:04 +0700

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -11,12 +11,14 @@ import java.io.IOException;
 public class SqlRuParse {
     public static void main(String[] args) throws IOException {
         DateTimeParser parser = new SqlRuDateTimeParser();
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Element forumTable = doc.select(".forumTable").first();
-        Element tableBody = forumTable.child(0);
-        for (Element tableRow : tableBody.children()) {
-            Element dateCell = tableRow.child(5);
-            System.out.println(parser.parse(dateCell.text()));
+        for (int index = 1; index < 6; index++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + index).get();
+            Element forumTable = doc.select(".forumTable").first();
+            Element tableBody = forumTable.child(0);
+            for (Element tableRow : tableBody.children()) {
+                Element dateCell = tableRow.child(5);
+                System.out.println(parser.parse(dateCell.text()));
+            }
         }
     }
 }
