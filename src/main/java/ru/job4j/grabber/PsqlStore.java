f02ae24196b0613c10d55caea7545f0dbd5d1abb
RaduKostashchuk <kostasc@mail.ru>
2021-10-14 20:54:07 +0700

    6. Grabber. [#289477]

--- src/main/java/ru/job4j/grabber/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -28,7 +28,7 @@ public class PsqlStore implements Store, AutoCloseable {
     public void save(Post post) {
     try (PreparedStatement statement = cnn.prepareStatement(
             "insert into posts(name, description, link, created, updated)"
-                    + "values (?, ?, ?, ?, ?)")) {
+                    + "values (?, ?, ?, ?, ?) on conflict do nothing")) {
         statement.setString(1, post.getTitle());
         statement.setString(2, post.getDescription());
         statement.setString(3, post.getLink());
